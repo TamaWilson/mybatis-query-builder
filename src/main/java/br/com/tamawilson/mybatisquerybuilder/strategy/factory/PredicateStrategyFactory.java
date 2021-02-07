dace246977576a3e53bf4c0464c4045d9f6a3c8a
TamaWilson <wilson_tama@hotmail.com>
2021-02-07 13:27:57 -0300

    refactor(PredicateStrategyFactory): Correting PredicateStrategyFactory to be static instance instead of be a spring component.

--- src/main/java/br/com/tamawilson/mybatisquerybuilder/strategy/factory/PredicateStrategyFactory.java
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/strategy/factory/PredicateStrategyFactory.java
@@ -1,24 +1,32 @@
 package br.com.tamawilson.mybatisquerybuilder.strategy.factory;
 
+import br.com.tamawilson.mybatisquerybuilder.strategy.BasicOperationsStrategy;
+import br.com.tamawilson.mybatisquerybuilder.strategy.NumericOperationsStrategy;
+import br.com.tamawilson.mybatisquerybuilder.strategy.StringOperationsStrategy;
 import br.com.tamawilson.mybatisquerybuilder.strategy.enums.PredicateStrategyType;
 import br.com.tamawilson.mybatisquerybuilder.strategy.interfaces.PredicateStrategy;
 import br.com.tamawilson.mybatisquerybuilder.utils.NumberReflectionsUtil;
-import org.springframework.stereotype.Component;
 
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.Set;
 
-@Component
 public class PredicateStrategyFactory {
 
-    private Map<PredicateStrategyType, PredicateStrategy> strategies;
-
-    public PredicateStrategyFactory(Set<PredicateStrategy> strategiesSet) {
-        createStrategy(strategiesSet);
+    private PredicateStrategyFactory() {
     }
 
-    public PredicateStrategy findStrategy(Class<?> clazz) {
+    private static final Map<PredicateStrategyType, PredicateStrategy> strategies = new EnumMap<>(PredicateStrategyType.class);
+
+    static {
+        strategies.put(PredicateStrategyType.BASIC_OPERATIONS,
+                new BasicOperationsStrategy());
+        strategies.put(PredicateStrategyType.NUMERIC_OPERATIONS,
+                new NumericOperationsStrategy());
+        strategies.put(PredicateStrategyType.STRING_OPERATIONS,
+                new StringOperationsStrategy());
+    }
+
+    public static PredicateStrategy getStrategy(Class<?> clazz) {
         if(clazz.isAssignableFrom(String.class)){
             return strategies.get(PredicateStrategyType.STRING_OPERATIONS);
         }
@@ -30,10 +38,6 @@ public class PredicateStrategyFactory {
         return strategies.get(PredicateStrategyType.BASIC_OPERATIONS);
     }
 
-    private void createStrategy(Set<PredicateStrategy> strategySet) {
-        strategies = new EnumMap<>(PredicateStrategyType.class);
-        strategySet.forEach(
-                strategy ->strategies.put(strategy.getStrategyType(), strategy));
-    }
+
 
 }
