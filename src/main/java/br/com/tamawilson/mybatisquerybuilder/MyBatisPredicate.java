2e285de77fc98e3b293a93be099b66a5ce239341
TamaWilson <wilson_tama@hotmail.com>
2021-02-07 13:28:43 -0300

    refactor(MyBatisPredicate): Removind autorwire for PredicateStategyFactory and changing generatePredicate() to use the new factory implementation

--- src/main/java/br/com/tamawilson/mybatisquerybuilder/MyBatisPredicate.java
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/MyBatisPredicate.java
@@ -5,23 +5,15 @@ import br.com.tamawilson.mybatisquerybuilder.model.MappedField;
 import br.com.tamawilson.mybatisquerybuilder.model.dto.SearchCriteria;
 import br.com.tamawilson.mybatisquerybuilder.strategy.factory.PredicateStrategyFactory;
 import br.com.tamawilson.mybatisquerybuilder.strategy.interfaces.PredicateStrategy;
-import org.springframework.stereotype.Component;
 
 import static br.com.tamawilson.mybatisquerybuilder.utils.MyBatisColumnUtil.extractColumn;
 
 
-@Component
 public class MyBatisPredicate {
 
     private StringBuilder predicate;
     private SearchCriteria criteria;
     private Class<?> clazz;
-
-    private final PredicateStrategyFactory predicateStrategyFactory;
-
-    public MyBatisPredicate(PredicateStrategyFactory predicateStrategyFactory) {
-        this.predicateStrategyFactory = predicateStrategyFactory;
-    }
 
     public MyBatisPredicate setCriteria(final SearchCriteria criteria) {
         this.criteria = criteria;
@@ -41,7 +33,7 @@ public class MyBatisPredicate {
 
         MappedField mappedField = extractColumn(criteria.getKey(), clazz);
 
-        PredicateStrategy predicateStrategy = predicateStrategyFactory.findStrategy(mappedField.getTypeAs());
+        PredicateStrategy predicateStrategy = PredicateStrategyFactory.getStrategy(mappedField.getTypeAs());
         String stringCriteria = predicateStrategy.getPredicate(criteria, mappedField);
         predicate = new StringBuilder(stringCriteria);
 
