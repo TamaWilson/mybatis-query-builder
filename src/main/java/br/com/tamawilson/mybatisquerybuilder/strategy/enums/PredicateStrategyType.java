e55e09cff974b05f6d0f149dfe6077befeee436f
TamaWilson <wilson_tama@hotmail.com>
2021-02-06 21:53:17 -0300

    feat(PredicateStrategyType): creating enum for avaiable predicate strategies

---
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/strategy/enums/PredicateStrategyType.java
@@ -0,0 +1,14 @@
+package br.com.tamawilson.mybatisquerybuilder.strategy.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum PredicateStrategyType {
+
+    BASIC_OPERATIONS("basic_operations"),
+    STRING_OPERATIONS("string_operations");
+
+    @Getter
+    private final String typeName;
+}
