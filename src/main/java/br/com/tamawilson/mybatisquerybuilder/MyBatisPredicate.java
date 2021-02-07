c0c94280de63f4849791228ac2fb44d82dd1bacb
TamaWilson <wilson_tama@hotmail.com>
2021-02-06 22:19:21 -0300

    feat(MyBatisPredicate): Changing class to use operations strategies

--- src/main/java/br/com/tamawilson/mybatisquerybuilder/MyBatisPredicate.java
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/MyBatisPredicate.java
@@ -3,20 +3,25 @@ package br.com.tamawilson.mybatisquerybuilder;
 
 import br.com.tamawilson.mybatisquerybuilder.model.MappedField;
 import br.com.tamawilson.mybatisquerybuilder.model.dto.SearchCriteria;
-import br.com.tamawilson.mybatisquerybuilder.model.exception.MyBatisColumnException;
-
-import java.lang.reflect.Field;
+import br.com.tamawilson.mybatisquerybuilder.strategy.factory.PredicateStrategyFactory;
+import br.com.tamawilson.mybatisquerybuilder.strategy.interfaces.PredicateStrategy;
+import org.springframework.stereotype.Component;
 
 import static br.com.tamawilson.mybatisquerybuilder.utils.MyBatisColumnUtil.extractColumn;
-import static br.com.tamawilson.mybatisquerybuilder.utils.NumberReflectionsUtil.isReflectedAsNumber;
 
 
+@Component
 public class MyBatisPredicate {
 
-    private final StringBuilder predicate = new StringBuilder();
+    private StringBuilder predicate;
     private SearchCriteria criteria;
     private Class<?> clazz;
-    private Field field;
+
+    private final PredicateStrategyFactory predicateStrategyFactory;
+
+    public MyBatisPredicate(PredicateStrategyFactory predicateStrategyFactory) {
+        this.predicateStrategyFactory = predicateStrategyFactory;
+    }
 
     public MyBatisPredicate setCriteria(final SearchCriteria criteria) {
         this.criteria = criteria;
@@ -35,71 +40,12 @@ public class MyBatisPredicate {
     public MyBatisPredicate generatePredicate() {
 
         MappedField mappedField = extractColumn(criteria.getKey(), clazz);
-        field = mappedField.getField();
 
-        if (isReflectedAsNumber(mappedField.getTypeAs())) {
-            appendNumericPredicate(mappedField.getColumnName());
-        }
-
-        if (mappedField.getTypeAs().isAssignableFrom(String.class)) {
-            appendStringPredicate(mappedField.getColumnName());
-        }
+        PredicateStrategy predicateStrategy = predicateStrategyFactory.findStrategy(mappedField.getTypeAs());
+        String stringCriteria = predicateStrategy.getPredicate(criteria, mappedField);
+        predicate = new StringBuilder(stringCriteria);
 
         return this;
-    }
-
-    private void appendNumericPredicate(String currentColumn) {
-        switch (criteria.getOperation()) {
-            case ":":
-                String equals = String.format("%s = %s", currentColumn, criteria.getValue());
-                predicate.append(equals);
-                break;
-            case ">":
-                String greaterThan = String.format("%s > %s", currentColumn, criteria.getValue());
-                predicate.append(greaterThan);
-                break;
-            case "<":
-                String lowerThan = String.format("%s < %s", currentColumn, criteria.getValue());
-                predicate.append(lowerThan);
-                break;
-            case "!:":
-                String different = String.format("%s != %s", currentColumn, criteria.getValue());
-                predicate.append(different);
-                break;
-            case "::":
-                String in = String.format("%s in %s", currentColumn, criteria.getValue());
-                predicate.append(in);
-                break;
-            case "!::":
-                String notIn = String.format("%s not in %s", currentColumn, criteria.getValue());
-                predicate.append(notIn);
-                break;
-            default:
-                throw new MyBatisColumnException(criteria.getKey(), criteria.getOperation(), field.getType().toString());
-        }
-    }
-
-    private void appendStringPredicate(String currentColumn) {
-        switch (criteria.getOperation()) {
-            case ":":
-                String equals = String.format("unaccent(%s) = unaccent('%s')", currentColumn, criteria.getValue());
-                predicate.append(equals);
-                break;
-            case ":*":
-                String contains = String.format("unaccent(%s) ilike unaccent('%%%s%%')", currentColumn, criteria.getValue());
-                predicate.append(contains);
-                break;
-            case ">*":
-                String startWith = String.format("unaccent(%s) ilike unaccent('%s%%')", currentColumn, criteria.getValue());
-                predicate.append(startWith);
-                break;
-            case "*<":
-                String endsWith = String.format("unaccent(%s) ilike unaccent('%%%s')", currentColumn, criteria.getValue());
-                predicate.append(endsWith);
-                break;
-            default:
-                throw new MyBatisColumnException(criteria.getKey(), criteria.getOperation(), field.getType().toString());
-        }
     }
 
     public void addAgregator() {
