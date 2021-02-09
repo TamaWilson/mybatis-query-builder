106ddd8af87edff3cffe35442c84b65b521564a3
Wilson <willsontama@gmail.com>
2021-02-09 16:38:34 -0300

    fix(StringPredicatePatterns): Cleaning code

--- src/main/java/br/com/tamawilson/mybatisquerybuilder/model/patterns/StringPredicatePatterns.java
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/model/patterns/StringPredicatePatterns.java
@@ -2,11 +2,11 @@ package br.com.tamawilson.mybatisquerybuilder.model.patterns;
 
 public class StringPredicatePatterns {
 
-    private StringPredicatePatterns() {
-    }
-
     public static final String EQUAL = "unaccent(%s) = unaccent('%s')";
     public static final String CONTAINS = "unaccent(%s) ilike unaccent('%%%s%%')";
     public static final String STARTS_WITH = "unaccent(%s) ilike unaccent('%s%%')";
     public static final String ENDS_WITH = "unaccent(%s) ilike unaccent('%%%s')";
+
+    private StringPredicatePatterns() {
+    }
 }
