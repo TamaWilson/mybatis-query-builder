3946a1cd7448fc281450a3a0775d20c6b9751c5a
Wilson <willsontama@gmail.com>
2021-02-22 09:07:20 -0300

    feat(StringPredicatePatterns): Fixing patterns

--- src/main/java/br/com/tamawilson/mybatisquerybuilder/model/patterns/StringPredicatePatterns.java
+++ src/main/java/br/com/tamawilson/mybatisquerybuilder/model/patterns/StringPredicatePatterns.java
@@ -2,10 +2,10 @@ package br.com.tamawilson.mybatisquerybuilder.model.patterns;
 
 public class StringPredicatePatterns {
 
-    public static final String EQUAL = "unaccent({0}}) = unaccent('%' || {1} || '%')";
+    public static final String EQUAL = "unaccent({0}}) = unaccent({1})";
     public static final String CONTAINS = "unaccent({0}) ilike unaccent('%' || {1} || '%')";
-    public static final String STARTS_WITH = "unaccent({0}) ilike unaccent('%' || {1} || '%')";
-    public static final String ENDS_WITH = "unaccent({0}) ilike unaccent('%' || {1} || '%')";
+    public static final String STARTS_WITH = "unaccent({0}) ilike unaccent({1} || '%')";
+    public static final String ENDS_WITH = "unaccent({0}) ilike unaccent('%' || {1})";
 
     private StringPredicatePatterns() {
     }
